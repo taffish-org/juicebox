9ff09dfd0e5cd671aa53b7763c715399d72732bd
Kaiyuan Han <hermit_han@163.com>
2025-04-22 19:07:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-juicebox
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/aidenlab/JuiceboxGUI
+
+Juicebox is visualization software for Hi-C data.
---
+++ juicebox.info.lisp
@@ -0,0 +1,8 @@
+(:name           "juicebox")
+(:repo           "taffish-org/juicebox")
+(:author         "KaiyuanHan")
+(:version        "3.1.4")
+(:intro          "Juicebox is visualization software for Hi-C data.")
+(:dependencies)
+(:functions      "juicebox"
+                 "[jdk]")
